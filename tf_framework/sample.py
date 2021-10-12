cede725ad864775050206f1ea59503345807be15
Jonas Gacrama <gacrama.jonas@gmail.com>
2021-10-12 16:23:23 +0800

    Added sample code to plot accuracy

---
+++ tf_framework/sample.py
@@ -0,0 +1,12 @@
+# Sample code for plotting accuracy
+
+loss_train = history.history['acc']
+loss_val = history.history['val_acc']
+epochs = range(1,11)
+plt.plot(epochs, loss_train, 'g', label='Training accuracy')
+plt.plot(epochs, loss_val, 'b', label='validation accuracy')
+plt.title('Training and Validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
